9e1dc0f34d14c602cb21bfbdcf1b6189b08a661e
sardylan <sardylan@gmail.com>
2019-07-26 23:52:42 +0200

    Refactoring custom timezone widget

--- widget_datetime_tz/static/src/js/fields/datetime_tz.js
+++ widget_datetime_tz/static/src/js/fields/datetime_tz.js
@@ -1,35 +1,38 @@
 odoo.define('widget_datetime_tz.basic_fields', function (require) {
     "use strict";
 
-    var registry = require('web.field_registry');
-    var fields = require('web.basic_fields');
+    const registry = require('web.field_registry');
+    const fields = require('web.basic_fields');
 
-    var FieldDateTimeTZ = fields.FieldDateTime.extend({
+    const FieldDateTimeTZ = fields.FieldDateTime.extend({
         supportedFieldTypes: ['datetime'],
 
         init: function () {
             this._super.apply(this, arguments);
             this.formatOptions.timezone = false;
             if (this.value) {
-                // var offset = this.getSession().getTZOffset(this.value);
-                var offset = 0;
-                var displayedValue = this.value.clone().add(offset, 'minutes');
-                this.datepickerOptions.defaultDate = displayedValue;
+                let offset = this._getTimezoneOffset();
+                this.datepickerOptions.defaultDate = this.value.clone().add(offset, 'minutes');
             }
         },
+
         _getValue: function () {
-            var value = this.datewidget.getValue();
-            // var offset = -this.getSession().getTZOffset(value);
-            var offset = 0;
+            let value = this.datewidget.getValue();
+            let offset = -this._getTimezoneOffset();
             return value && value.add(offset, 'minutes');
         },
+
         _renderEdit: function () {
-            // var offset = this.getSession().getTZOffset(this.value);
-            var offset = 0;
-            var value = this.value && this.value.clone().add(offset, 'minutes');
+            let offset = this._getTimezoneOffset();
+            let value = this.value && this.value.clone().add(offset, 'minutes');
             this.datewidget.setValue(value);
             this.$input = this.datewidget.$input;
         },
+
+        _getTimezoneOffset: function () {
+            // return this.getSession().getTZOffset(this.value);
+            return 0;
+        }
     });
 
     registry.add('datetime_tz', FieldDateTimeTZ);
